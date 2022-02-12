998099323b60a237d2cdbe8a6c93c95aed6d37bf
jeffbushdesign <jeffbushdesign@gmail.com>
2022-02-12 11:26:22 -0500

    Test

--- nameInverter.js
+++ nameInverter.js
@@ -1,9 +1,28 @@
-const nameInverter = function(name) {
-  if (name === '') {
+const nameInverter = function (name) {
+  if (name === undefined) {
+    return 'Error';
+  }
+  const nameArr = name.trim().split(' ');
+  if (nameArr.length === 0) {
     return '';
   }
-  return name;
-
-}
+  if (nameArr.length === 1) {
+    if (nameArr[0].includes('.')) {
+      return '';
+    } else {
+      return nameArr[0];
+    }
+  }
+  if (nameArr.length === 2) {
+    if (nameArr[0].includes('.')) {
+      return `${nameArr[0]} ${nameArr[1]}`;
+    } else {
+      return `${nameArr[1]}, ${nameArr[0]}`;
+    }
+  }
+  if (nameArr.length === 3) {
+    return `${nameArr[0]} ${nameArr[2]}, ${nameArr[1]}`;
+  }
+};
 
 module.exports = nameInverter;
--- test/nameInverterTest.js
+++ test/nameInverterTest.js
@@ -3,11 +3,76 @@ const assert = chai.assert;
 
 const nameInverter = require('../nameInverter');
 
-describe('nameInverter', function() {
-  it('should return an empty string when passed an empty string', function() {
+describe('nameInverter', function () {
+  it('should return an empty string when passed an empty string', function () {
     const inputName = "";
     const expectedOutput = "";
     assert.equal(nameInverter(inputName), expectedOutput);
   });
+
+  it('should return a single name when passed a single name', function () {
+    const inputName = "name";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
+
+  it('should return empty string when passed only honorific', function () {
+    const inputName = "Mr.";
+    const expectedOutput = "";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
+
+  it('should return a single name when passed a single name with extra spaces', function () {
+    const inputName = " name ";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
+
+  it('should return a last-name, first-name when passed a first and last-name', function () {
+    const inputName = "first last";
+    const expectedOutput = "last, first";
+    assert.equal(nameInverter(inputName), expectedOutput);
+
+  });
+
+  it('should return honorific first-name when passed honorific first-name', function () {
+    const inputName = "Mr. Andrea";
+    const expectedOutput = "Mr. Andrea";
+    assert.equal(nameInverter(inputName), expectedOutput);
+
+  });
+
+  it('should return an empty string when passed a single honorific', function () {
+    const inputName = "Dr.";
+    const expectedOutput = "";
+    assert.equal(nameInverter(inputName), expectedOutput);
+
+  });
+
+  it('should return honorific last-name, first-name when passed honorific first-name last-name', function () {
+    const inputName = "Mr. Andrea Bocelli";
+    const expectedOutput = "Mr. Bocelli, Andrea";
+    assert.equal(nameInverter(inputName), expectedOutput);
+
+  });
+
+  it('should throw an error when name is undefined', function () {
+    const inputName = undefined;
+    const expectedOutput = "Error";
+    assert.equal(nameInverter(inputName), expectedOutput);
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
